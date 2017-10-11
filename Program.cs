bde00f1e9e0f006166697e12996010dbf630c086
Bernhard Richter <bernhard.richter@gmail.com>
2017-10-11 14:51:17 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace travistest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
